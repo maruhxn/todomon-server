35b67a20e98e7d7c47bb980a3bc59ae69263bbb6
maruhxn <maruhan1016@gmail.com>
2024-07-17 00:11:55 +0900

    test: Test Feed Logic

--- src/test/java/com/maruhxn/todomon/domain/pet/application/PetServiceTest.java
+++ src/test/java/com/maruhxn/todomon/domain/pet/application/PetServiceTest.java
@@ -7,6 +7,7 @@ import com.maruhxn.todomon.domain.pet.domain.Pet;
 import com.maruhxn.todomon.domain.pet.domain.PetType;
 import com.maruhxn.todomon.domain.pet.domain.Rarity;
 import com.maruhxn.todomon.domain.pet.dto.request.CreatePetReq;
+import com.maruhxn.todomon.domain.pet.dto.request.FeedReq;
 import com.maruhxn.todomon.global.auth.model.Role;
 import com.maruhxn.todomon.global.auth.model.provider.OAuth2Provider;
 import com.maruhxn.todomon.global.error.ErrorCode;
@@ -19,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+import static com.maruhxn.todomon.global.common.Constants.PET_GAUGE_INCREASE_RATE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -88,6 +90,105 @@ class PetServiceTest extends IntegrationTestSupport {
         // when / then
         assertThatThrownBy(() -> petService.create(member, req))
                 .hasMessage(ErrorCode.NO_SPACE_PET_HOUSE.getMessage())
+                .isInstanceOf(BadRequestException.class);
+    }
+
+    @Test
+    @DisplayName("펫에게 먹이를 주면, 게이지가 오르고, 유저의 보유 먹이 수는 감소한다.")
+    void feed() {
+        // given
+        Pet pet = Pet.builder()
+                .petType(PetType.getRandomPetType())
+                .rarity(Rarity.COMMON)
+                .build();
+        member.addPet(pet);
+        member.addFood(10);
+        petRepository.save(pet);
+
+        FeedReq req = new FeedReq(1);
+
+        // when
+        petService.feed(pet.getId(), member, req);
+
+        // then
+        assertThat(pet)
+                .extracting("level", "gauge", "evolutionCnt")
+                .containsExactly(1, PET_GAUGE_INCREASE_RATE, 0);
+        assertThat(member.getFoodCnt()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("펫에게 먹이를 주어 게이지가 오를 때, 100%를 초과하면 레벨업을 한다.")
+    void feedWithLevelUp() {
+        // given
+        Pet pet = Pet.builder()
+                .petType(PetType.getRandomPetType())
+                .rarity(Rarity.COMMON)
+                .build();
+        pet.increaseGauge(99.0);
+        member.addPet(pet);
+        member.addFood(10);
+        petRepository.save(pet);
+
+        FeedReq req = new FeedReq(1);
+
+        // when
+        petService.feed(pet.getId(), member, req);
+
+        // then
+        assertThat(pet)
+                .extracting("level", "gauge", "evolutionCnt")
+                .containsExactly(2, (99.0 + PET_GAUGE_INCREASE_RATE) % 100, 0);
+        assertThat(member.getFoodCnt()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("펫에게 먹이를 주어 게이지가 오를 때, 레벨업을 하여 30Lv이 넘을 경우, 진화한다.")
+    void feedWithEvolution() {
+        // given
+        Pet pet = Pet.builder()
+                .petType(PetType.getRandomPetType())
+                .rarity(Rarity.COMMON)
+                .build();
+        String prevAppearance = pet.getAppearance();
+        for (int i = 0; i < 28; i++) {
+            pet.levelUp();
+        }
+
+        pet.increaseGauge(80.0);
+        member.addPet(pet);
+        member.addFood(10);
+        petRepository.save(pet);
+
+        FeedReq req = new FeedReq(10);
+
+        // when
+        petService.feed(pet.getId(), member, req);
+
+        // then
+        assertThat(prevAppearance).isNotEqualTo(pet.getAppearance());
+        assertThat(pet)
+                .extracting("level", "gauge", "evolutionCnt")
+                .containsExactly(30, 0.0, 1);
+    }
+
+    @Test
+    @DisplayName("펫에게 먹이를 줄 때, 요청된 먹이 수가 보유한 먹이 수보다 많을 경우 에러가 발생한다.")
+    void feedFailByOverFood() {
+        // given
+        Pet pet = Pet.builder()
+                .petType(PetType.getRandomPetType())
+                .rarity(Rarity.COMMON)
+                .build();
+        member.addPet(pet);
+        member.addFood(1);
+        petRepository.save(pet);
+
+        FeedReq req = new FeedReq(10);
+
+        // when
+        assertThatThrownBy(() -> petService.feed(pet.getId(), member, req))
+                .hasMessage(ErrorCode.OVER_FOOD_CNT.getMessage())
                 .isInstanceOf(BadRequestException.class);
     }
 
