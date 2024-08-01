bb9c73da49fb60ec60af0ea7addac766fd42c8dd
maruhxn <maruhan1016@gmail.com>
2024-08-01 16:06:52 +0900

    test: Test StarTransactionControllerIntegrationTest

--- src/main/java/com/maruhxn/todomon/domain/social/api/StarTransactionController.java
+++ src/main/java/com/maruhxn/todomon/domain/social/api/StarTransactionController.java
@@ -2,7 +2,9 @@ package com.maruhxn.todomon.domain.social.api;
 
 import com.maruhxn.todomon.domain.social.application.StarTransactionService;
 import com.maruhxn.todomon.global.auth.model.TodomonOAuth2User;
+import com.maruhxn.todomon.global.common.dto.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +17,17 @@ public class StarTransactionController {
 
     // 내가 팔로우하고 있는 사람에게 별을 보낼 수 있다.
     @PostMapping("/send/{memberId}")
-    public void sendStar(
+    @ResponseStatus(HttpStatus.CREATED)
+    public BaseResponse sendStar(
             @AuthenticationPrincipal TodomonOAuth2User todomonOAuth2User,
             @PathVariable("memberId") Long memberId
     ) {
         starTransactionService.sendStar(todomonOAuth2User.getMember(), memberId);
+        return new BaseResponse("별 보내기 성공 - " + memberId);
     }
 
     @PatchMapping("/receive/{transactionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void receiveStar(
             @AuthenticationPrincipal TodomonOAuth2User todomonOAuth2User,
             @PathVariable("transactionId") Long transactionId
@@ -31,6 +36,7 @@ public class StarTransactionController {
     }
 
     @PatchMapping("/receiveAll")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void receiveAllStars(
             @AuthenticationPrincipal TodomonOAuth2User todomonOAuth2User
     ) {
