4a131ec23a2aa158e42a501bae1b1a11ed65eb92
maruhxn <maruhan1016@gmail.com>
2024-09-03 15:55:13 +0900

    chore: fix build.gradle + Add Config

--- todomon-batch/src/main/java/com/maruhxn/todomon/batch/TodomonBatchApplication.java
+++ todomon-batch/src/main/java/com/maruhxn/todomon/batch/TodomonBatchApplication.java
@@ -2,8 +2,12 @@ package com.maruhxn.todomon.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.maruhxn.todomon") // 엔티티가 위치한 패키지
+@EnableJpaRepositories(basePackages = "com.maruhxn.todomon")
 public class TodomonBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodomonBatchApplication.class, args);
