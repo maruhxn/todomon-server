41ddb767336e84489d3849e7b216a925259eb53c
maruhxn <maruhan1016@gmail.com>
2024-09-21 03:00:50 +0900

    feat: Add IsTodayTodoOrAdmin AOP

---
+++ todomon-core/src/main/java/com/maruhxn/todomon/core/global/auth/checker/IsTodayTodoOrAdmin.java
@@ -0,0 +1,11 @@
+package com.maruhxn.todomon.core.global.auth.checker;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IsTodayTodoOrAdmin {
+}
