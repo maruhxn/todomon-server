e20f48ef70888d730382e937d9baf18967a89938
maruhxn <maruhan1016@gmail.com>
2024-10-02 02:25:31 +0900

    feat: SocialRankQueryRepository Query Tuning

--- todomon-core/src/main/java/com/maruhxn/todomon/core/domain/social/dao/SocialQueryRepository.java
+++ todomon-core/src/main/java/com/maruhxn/todomon/core/domain/social/dao/SocialQueryRepository.java
@@ -14,7 +14,10 @@ import org.springframework.stereotype.Repository;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.maruhxn.todomon.core.domain.member.domain.QDiligence.diligence;
 import static com.maruhxn.todomon.core.domain.member.domain.QMember.member;
@@ -30,7 +33,28 @@ public class SocialQueryRepository {
     private final JPAQueryFactory query;
 
     public List<DiligenceRankItem> findTop10MembersByDiligenceLevelAndGauge(Long currentMemberId) {
-        List<DiligenceRankItem> results = query
+        List<DiligenceRankItem> followeeRankings = query
+                .select(
+                        Projections.fields(DiligenceRankItem.class,
+                                member.id.as("memberId"),
+                                member.username,
+                                member.profileImageUrl,
+                                diligence.level,
+                                Projections.fields(AbstractMemberInfoItem.TitleNameItem.class,
+                                        titleName.name,
+                                        titleName.color
+                                ).as("title")
+                        )
+                )
+                .from(follow)
+                .innerJoin(member).on(follow.followee.id.eq(member.id).and(isAccepted()).and(follow.follower.id.eq(currentMemberId)))
+                .innerJoin(member.diligence, diligence)
+                .leftJoin(member.titleName, titleName)
+                .orderBy(diligence.level.desc(), diligence.gauge.desc(), member.createdAt.asc())
+                .limit(10)
+                .fetch();
+
+        DiligenceRankItem myItem = query
                 .select(
                         Projections.fields(DiligenceRankItem.class,
                                 member.id.as("memberId"),
@@ -44,16 +68,12 @@ public class SocialQueryRepository {
                         )
                 )
                 .from(member)
-                .leftJoin(follow).on(follow.followee.id.eq(member.id).and(isAccepted()))
-                .join(member.diligence, diligence)
+                .innerJoin(member.diligence, diligence)
                 .leftJoin(member.titleName, titleName)
-                .where(
-                        followerIsCurrentMember(currentMemberId)
-                                .or(isCurrentMember(currentMemberId))
-                )
-                .orderBy(diligence.level.desc(), diligence.gauge.desc(), member.createdAt.asc())
-                .limit(10)
-                .fetch();
+                .where(member.id.eq(currentMemberId))
+                .fetchOne();
+
+        List<DiligenceRankItem> results = concatAndSortTodoDiligenceRankItems(followeeRankings, myItem);
 
         results.forEach(AbstractMemberInfoItem::setTitleToNullIfIsEmpty);
 
@@ -61,7 +81,29 @@ public class SocialQueryRepository {
     }
 
     public List<CollectedPetRankItem> findTop10MembersByCollectedPetCnt(Long currentMemberId) {
-        List<CollectedPetRankItem> results = query.select(
+        List<CollectedPetRankItem> followeeRankings = query.select(
+                        Projections.fields(CollectedPetRankItem.class,
+                                member.id.as("memberId"),
+                                member.username,
+                                member.profileImageUrl,
+                                collectedPet.id.count().intValue().as("petCnt"),
+                                collectedPet.createdAt.max().as("lastCollectedAt"),
+                                Projections.fields(AbstractMemberInfoItem.TitleNameItem.class,
+                                        titleName.name,
+                                        titleName.color
+                                ).as("title")
+                        )
+                )
+                .from(follow)
+                .innerJoin(member).on(follow.followee.id.eq(member.id).and(isAccepted()).and(follow.follower.id.eq(currentMemberId)))
+                .leftJoin(member.collectedPets, collectedPet)
+                .leftJoin(member.titleName, titleName)
+                .groupBy(member.id)
+                .orderBy(collectedPet.id.count().desc(), collectedPet.createdAt.max().desc(), member.createdAt.asc())
+                .limit(10)
+                .fetch();
+
+        CollectedPetRankItem myItem = query.select(
                         Projections.fields(CollectedPetRankItem.class,
                                 member.id.as("memberId"),
                                 member.username,
@@ -75,17 +117,13 @@ public class SocialQueryRepository {
                         )
                 )
                 .from(member)
-                .leftJoin(follow).on(follow.followee.id.eq(member.id).and(isAccepted()))
                 .leftJoin(member.collectedPets, collectedPet)
                 .leftJoin(member.titleName, titleName)
-                .where(
-                        followerIsCurrentMember(currentMemberId)
-                                .or(isCurrentMember(currentMemberId))
-                )
-                .groupBy(member.id, member.username, member.profileImageUrl)
-                .orderBy(collectedPet.id.count().desc(), collectedPet.createdAt.max().desc(), member.createdAt.asc())
-                .limit(10)
-                .fetch();
+                .where(member.id.eq(currentMemberId))
+                .groupBy(member.id)
+                .fetchOne();
+
+        List<CollectedPetRankItem> results = concatAndSortTodoCollectedPetRankItems(followeeRankings, myItem);
 
         results.forEach(AbstractMemberInfoItem::setTitleToNullIfIsEmpty);
 
@@ -95,7 +133,29 @@ public class SocialQueryRepository {
     public List<TodoAchievementRankItem> findTop10MembersByYesterdayAchievement(Long currentMemberId) {
         LocalDate yesterday = LocalDate.now().minusDays(1);
 
-        List<TodoAchievementRankItem> results = query.select(
+        List<TodoAchievementRankItem> followeeRankings = query.select(
+                        Projections.fields(TodoAchievementRankItem.class,
+                                member.id.as("memberId"),
+                                member.username,
+                                member.profileImageUrl,
+                                todoAchievementHistory.cnt,
+                                Projections.fields(AbstractMemberInfoItem.TitleNameItem.class,
+                                        titleName.name,
+                                        titleName.color
+                                ).as("title")
+                        )
+                )
+                .from(follow)
+                .innerJoin(member).on(follow.followee.id.eq(member.id).and(isAccepted()).and(follow.follower.id.eq(currentMemberId)))
+                .leftJoin(todoAchievementHistory).on(todoAchievementHistory.memberId.eq(member.id))
+                .leftJoin(member.titleName, titleName)
+                .where(todoAchievementHistory.date.eq(yesterday))
+                .groupBy(member.id, todoAchievementHistory.cnt)
+                .orderBy(todoAchievementHistory.cnt.desc(), todoAchievementHistory.createdAt.max().asc(), member.createdAt.asc())
+                .limit(10)
+                .fetch();
+
+        TodoAchievementRankItem myItem = query.select(
                         Projections.fields(TodoAchievementRankItem.class,
                                 member.id.as("memberId"),
                                 member.username,
@@ -108,20 +168,13 @@ public class SocialQueryRepository {
                         )
                 )
                 .from(member)
-                .leftJoin(follow).on(follow.followee.id.eq(member.id).and(isAccepted()))
-                .join(todoAchievementHistory).on(todoAchievementHistory.memberId.eq(member.id))
+                .leftJoin(todoAchievementHistory).on(todoAchievementHistory.memberId.eq(member.id))
                 .leftJoin(member.titleName, titleName)
-                .where(
-                        (
-                                followerIsCurrentMember(currentMemberId)
-                                        .or(isCurrentMember(currentMemberId))
-                        )
-                                .and(todoAchievementHistory.date.eq(yesterday))
-                )
                 .groupBy(member.id, todoAchievementHistory.cnt)
-                .orderBy(todoAchievementHistory.cnt.sum().desc(), todoAchievementHistory.createdAt.max().asc(), member.createdAt.asc())
-                .limit(10)
-                .fetch();
+                .where(todoAchievementHistory.date.eq(yesterday).and(member.id.eq(currentMemberId)))
+                .fetchOne();
+
+        List<TodoAchievementRankItem> results = concatAndSortTodoAchievementRankItems(followeeRankings, myItem);
 
         results.forEach(AbstractMemberInfoItem::setTitleToNullIfIsEmpty);
 
@@ -137,12 +190,34 @@ public class SocialQueryRepository {
         LocalDate startOfLastWeek = startOfCurrentWeek.minusWeeks(1);
         LocalDate endOfLastWeek = startOfCurrentWeek.minusDays(1);
 
-        List<TodoAchievementRankItem> results = query.select(
+        List<TodoAchievementRankItem> followeeRankings = query.select(
                         Projections.fields(TodoAchievementRankItem.class,
                                 member.id.as("memberId"),
                                 member.username,
                                 member.profileImageUrl,
-                                todoAchievementHistory.cnt,
+                                todoAchievementHistory.cnt.sum().as("cnt"),
+                                Projections.fields(AbstractMemberInfoItem.TitleNameItem.class,
+                                        titleName.name,
+                                        titleName.color
+                                ).as("title")
+                        )
+                )
+                .from(follow)
+                .innerJoin(member).on(follow.followee.id.eq(member.id).and(isAccepted()).and(follow.follower.id.eq(currentMemberId)))
+                .leftJoin(todoAchievementHistory).on(todoAchievementHistory.memberId.eq(member.id))
+                .leftJoin(member.titleName, titleName)
+                .where(todoAchievementHistory.date.between(startOfLastWeek, endOfLastWeek))
+                .groupBy(member.id)
+                .orderBy(todoAchievementHistory.cnt.sum().desc(), todoAchievementHistory.createdAt.max().asc(), member.createdAt.asc())
+                .limit(10)
+                .fetch();
+
+        TodoAchievementRankItem myItem = query.select(
+                        Projections.fields(TodoAchievementRankItem.class,
+                                member.id.as("memberId"),
+                                member.username,
+                                member.profileImageUrl,
+                                todoAchievementHistory.cnt.sum().as("cnt"),
                                 Projections.fields(AbstractMemberInfoItem.TitleNameItem.class,
                                         titleName.name,
                                         titleName.color
@@ -150,22 +225,104 @@ public class SocialQueryRepository {
                         )
                 )
                 .from(member)
-                .leftJoin(follow).on(follow.followee.id.eq(member.id).and(isAccepted()))
-                .join(todoAchievementHistory).on(todoAchievementHistory.memberId.eq(member.id))
+                .leftJoin(todoAchievementHistory).on(todoAchievementHistory.memberId.eq(member.id))
                 .leftJoin(member.titleName, titleName)
-                .where(
-                        (
-                                followerIsCurrentMember(currentMemberId)
-                                        .or(isCurrentMember(currentMemberId))
-                        )
-                                .and(todoAchievementHistory.date.between(startOfLastWeek, endOfLastWeek))
-                )
-                .groupBy(member.id, todoAchievementHistory.cnt)
-                .orderBy(todoAchievementHistory.cnt.sum().desc(), todoAchievementHistory.createdAt.max().asc(), member.createdAt.asc())
-                .limit(10)
-                .fetch();
+                .groupBy(member.id)
+                .where(todoAchievementHistory.date.between(startOfLastWeek, endOfLastWeek).and(member.id.eq(currentMemberId)))
+                .fetchOne();
+
+        List<TodoAchievementRankItem> results = concatAndSortTodoAchievementRankItems(followeeRankings, myItem);
 
         results.forEach(AbstractMemberInfoItem::setTitleToNullIfIsEmpty);
+
+        return results;
+    }
+
+    private static List<CollectedPetRankItem> concatAndSortTodoCollectedPetRankItems(List<CollectedPetRankItem> followeeRankings, CollectedPetRankItem myItem) {
+        // null 체크 및 리스트 초기화
+        List<CollectedPetRankItem> combinedList = new ArrayList<>();
+
+        // followeeRankings가 null이 아닐 경우 추가
+        if (followeeRankings != null) {
+            combinedList.addAll(followeeRankings);
+        }
+
+        // myItem이 null이 아닐 경우 추가
+        if (myItem != null) {
+            combinedList.add(myItem);
+        }
+
+        // petCnt 내림차순, 같다면 memberId 오름차순 기준 정렬
+        List<CollectedPetRankItem> results = combinedList.stream()
+                .sorted(Comparator.comparingInt(CollectedPetRankItem::getPetCnt)
+                        .reversed() // petCnt를 기준으로 내림차순
+                        .thenComparing(
+                                CollectedPetRankItem::getLastCollectedAt,
+                                Comparator.nullsLast(Comparator.reverseOrder()) // lastCollectedAt을 기준으로 내림차순, null은 마지막에 위치
+                        )
+                        .thenComparingLong(CollectedPetRankItem::getMemberId) // memberId를 기준으로 오름차순
+                )
+                .collect(Collectors.toList());
+
+        if (results.size() > 10) {
+            results.remove(results.size() - 1); // 마지막 요소 제거
+        }
+
+        return results;
+    }
+
+    private static List<DiligenceRankItem> concatAndSortTodoDiligenceRankItems(List<DiligenceRankItem> followeeRankings, DiligenceRankItem myItem) {
+        // null 체크 및 리스트 초기화
+        List<DiligenceRankItem> combinedList = new ArrayList<>();
+
+        // followeeRankings가 null이 아닐 경우 추가
+        if (followeeRankings != null) {
+            combinedList.addAll(followeeRankings);
+        }
+
+        // myItem이 null이 아닐 경우 추가
+        if (myItem != null) {
+            combinedList.add(myItem);
+        }
+
+        // level 내림차순, 같다면 memberId 오름차순 기준 정렬
+        List<DiligenceRankItem> results = combinedList.stream()
+                .sorted(Comparator.comparingInt(DiligenceRankItem::getLevel)
+                        .reversed() // cnt를 기준으로 내림차순
+                        .thenComparingLong(DiligenceRankItem::getMemberId) // memberId를 기준으로 오름차순
+                ).collect(Collectors.toList());
+
+        if (results.size() > 10) {
+            results.remove(results.size() - 1); // 마지막 요소 제거
+        }
+
+        return results;
+    }
+
+    private static List<TodoAchievementRankItem> concatAndSortTodoAchievementRankItems(List<TodoAchievementRankItem> followeeRankings, TodoAchievementRankItem myItem) {
+        // null 체크 및 리스트 초기화
+        List<TodoAchievementRankItem> combinedList = new ArrayList<>();
+
+        // followeeRankings가 null이 아닐 경우 추가
+        if (followeeRankings != null) {
+            combinedList.addAll(followeeRankings);
+        }
+
+        // myItem이 null이 아닐 경우 추가
+        if (myItem != null) {
+            combinedList.add(myItem);
+        }
+
+        // cnt 내림차순, 같다면 memberId 오름차순 기준 정렬
+        List<TodoAchievementRankItem> results = combinedList.stream()
+                .sorted(Comparator.comparingLong(TodoAchievementRankItem::getCnt)
+                        .reversed() // cnt를 기준으로 내림차순
+                        .thenComparingLong(TodoAchievementRankItem::getMemberId) // memberId를 기준으로 오름차순
+                ).collect(Collectors.toList());
+
+        if (results.size() > 10) {
+            results.remove(results.size() - 1); // 마지막 요소 제거
+        }
 
         return results;
     }
