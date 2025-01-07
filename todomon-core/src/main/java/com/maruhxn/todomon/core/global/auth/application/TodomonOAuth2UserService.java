f5ba5bf302169577424dc6e129572e4a935f7fcb
maruhxn <maruhan1016@gmail.com>
2025-01-07 21:33:05 +0900

    refactor: OAuth2ProviderUser

--- todomon-core/src/main/java/com/maruhxn/todomon/core/global/auth/application/TodomonOAuth2UserService.java
+++ todomon-core/src/main/java/com/maruhxn/todomon/core/global/auth/application/TodomonOAuth2UserService.java
@@ -4,12 +4,7 @@ import com.maruhxn.todomon.core.domain.member.application.MemberService;
 import com.maruhxn.todomon.core.domain.member.domain.Member;
 import com.maruhxn.todomon.core.global.auth.dto.MemberDTO;
 import com.maruhxn.todomon.core.global.auth.model.TodomonOAuth2User;
-import com.maruhxn.todomon.core.global.auth.model.provider.GoogleUser;
-import com.maruhxn.todomon.core.global.auth.model.provider.KakaoUser;
-import com.maruhxn.todomon.core.global.auth.model.provider.NaverUser;
 import com.maruhxn.todomon.core.global.auth.model.provider.OAuth2ProviderUser;
-import com.maruhxn.todomon.core.global.error.ErrorCode;
-import com.maruhxn.todomon.core.global.error.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -36,7 +31,6 @@ public class TodomonOAuth2UserService implements OAuth2UserService<OAuth2UserReq
         OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest); // 인가 서버와 통신해서 실제 사용자 정보 조회
         OAuth2ProviderUser oAuth2ProviderUser = this.getOAuth2ProviderUser(clientRegistration, oAuth2User);
 
-        // 회원가입
         Member member = memberService.getOrCreate(oAuth2ProviderUser);
         MemberDTO dto = MemberDTO.from(member);
         return TodomonOAuth2User.of(dto, oAuth2ProviderUser);
@@ -45,15 +39,6 @@ public class TodomonOAuth2UserService implements OAuth2UserService<OAuth2UserReq
     private OAuth2ProviderUser getOAuth2ProviderUser(ClientRegistration clientRegistration, OAuth2User oAuth2User) {
         String registrationId = clientRegistration.getRegistrationId();
         Map<String, Object> attributes = oAuth2User.getAttributes();
-        switch (registrationId) {
-            case "google":
-                return new GoogleUser(attributes, registrationId);
-            case "naver":
-                return new NaverUser(attributes, registrationId);
-            case "kakao":
-                return new KakaoUser(attributes, registrationId);
-            default:
-                throw new BadRequestException(ErrorCode.BAD_REQUEST, "일치하는 제공자가 없습니다.");
-        }
+        return OAuth2ProviderUser.create(attributes, registrationId);
     }
 }
--- todomon-core/src/main/java/com/maruhxn/todomon/core/global/auth/model/provider/OAuth2ProviderUser.java
+++ todomon-core/src/main/java/com/maruhxn/todomon/core/global/auth/model/provider/OAuth2ProviderUser.java
@@ -1,14 +1,31 @@
 package com.maruhxn.todomon.core.global.auth.model.provider;
 
+import com.maruhxn.todomon.core.global.error.ErrorCode;
+import com.maruhxn.todomon.core.global.error.exception.BadRequestException;
+
 import java.util.Map;
 
 public abstract class OAuth2ProviderUser {
     private String provider;
     private final Map<String, Object> attributes;
 
-    public OAuth2ProviderUser(Map<String, Object> attributes, String registrationId) {
+    protected OAuth2ProviderUser(Map<String, Object> attributes, String registrationId) {
         this.attributes = attributes;
         this.provider = registrationId;
+    }
+
+    // 팩토리 메서드
+    public static OAuth2ProviderUser create(Map<String, Object> attributes, String registrationId) {
+        switch (registrationId) {
+            case "google":
+                return new GoogleUser(attributes, registrationId);
+            case "naver":
+                return new NaverUser(attributes, registrationId);
+            case "kakao":
+                return new KakaoUser(attributes, registrationId);
+            default:
+                throw new BadRequestException(ErrorCode.BAD_REQUEST, "일치하는 제공자가 없습니다.");
+        }
     }
 
     public abstract String getEmail();
